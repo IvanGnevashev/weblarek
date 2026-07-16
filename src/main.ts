d2b6fd41e0fa22e32fbc64d487ba2db814d7994d
Ivan Gnevashev <Eshkerik@yandex.ru>
2026-07-16 17:12:56 +0300

    начал делать presenter

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,48 @@
 import './scss/styles.scss';
-import { apiProducts } from './utils/data';
+
+import { CardCatalog } from './components/CardCatalog'
+import { Page } from './components/Page'
+import {  } from ''
+
 import { CatalogModel } from './components/Models/CatalogModel';
 import { BasketModel } from './components/Models/BasketModel';
 import { BuyerModel } from './components/Models/BuyerModel';
-import { LarekApi } from './components/api/LarekApi';
+
 import { Api } from './components/base/Api';
+import { LarekApi } from './components/api/LarekApi';
+
 import { API_URL } from './utils/constants';
 
-const catalog = new CatalogModel();
-const basket = new BasketModel();
-const buyer = new BuyerModel();
+const catalogModel = new CatalogModel();
+const basketModel = new BasketModel();
+const buyerModel = new BuyerModel();
+
 const baseApi = new Api(API_URL);
 const api = new LarekApi(baseApi);
 
-// --- Тестирование CatalogModel ---
-catalog.setItems(apiProducts.items);
-console.log('Каталог (мок):', catalog.getItems());
-console.log('Товар по id:', catalog.getItem('854cef69-976d-4c2a-a18c-2aa45046c390'));
+async function init() {
+    const response = await api.getProducts();
 
-catalog.setPreview(catalog.getItem('854cef69-976d-4c2a-a18c-2aa45046c390') ?? null);
-console.log('Превью товара:', catalog.getPreview());
+    catalogModel.setItems(response.items);
 
+    const cards = catalogModel.getItems().map( product => {
+        const container = cloneTemplate('#card-catalog');
+        const card = new CardCatalog(
+            container,
+            () => {
+                
+            }
+        );
+        return card.render(product);
+        }
+    );
 
-// --- Тестирование BasketModel ---
-basket.addItem(apiProducts.items[0]);
-basket.addItem(apiProducts.items[1]);
-console.log('Корзина:', basket.getItems());
-console.log('Количество:', basket.getCount());
-console.log('Сумма:', basket.getTotal());
-console.log('Товар в корзине:', basket.hasItem('854cef69-976d-4c2a-a18c-2aa45046c390'));
-
-basket.removeItem(apiProducts.items[0].id);
-console.log('После удаления:', basket.getItems());
-
-basket.clear();
-console.log('После очистки:', basket.getItems());
-
-// --- Тестирование BuyerModel ---
-buyer.setPayment('card');
-buyer.setAddress('ул. Пушкина');
-buyer.setEmail('test@test.ru');
-buyer.setPhone('+79991234567');
-
-console.log('Данные покупателя:', buyer.getData());
-console.log('Ошибки валидации:', buyer.validate());
-
-buyer.clear();
-console.log('После очистки:', buyer.getData());
-console.log('Ошибки:', buyer.validate());
-
-// --- Запрос к серверу ---
-api.getProducts()
-    .then((data) => {
-        catalog.setItems(data.items);
-        console.log('Каталог с сервера:', catalog.getItems());
+    page.render({
+        counter: 0,
+        catalog: cards
     })
-    .catch((err) => {
-        console.error('Ошибка получения каталога:', err);
-    });
+}
+
+init();
+
+const page = new Page(...)
